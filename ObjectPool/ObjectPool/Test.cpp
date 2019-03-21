192e6ef43419d99f3eaf60a755be953ece39b28e
zzh <zzh_Miss_Monster@163.com>
2019-03-21 14:26:46 +0800

    ObjectPool

---
+++ ObjectPool/ObjectPool/Test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+#include "ObjectPool.h"
+
+
+
+int main()
+{
+	////≤∂ªÒ“Ï≥£
+	//try
+	//{
+	//	TestObjectPool();
+	//}
+	//catch (const std::exception& e)
+	//{
+	//	cout << e.what() << endl;
+	//}
+
+	//BenchMark();
+	TestObjectPool();
+	system("pause");
+	return 0;
+}
