e7df8cd9638f52d4d0b2da65d36df72622a7fd82
zzh <zzh_Miss_Monster@163.com>
2019-04-09 14:15:21 +0800

    删除链表中的元素

---
+++ 删除链表中的元素/test.c
@@ -0,0 +1,38 @@
+//ÒÆ³ýÁ´±íÔªËØ
+//https://leetcode-cn.com/problems/remove-linked-list-elements/
+//Definition for singly-linked list.
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+ struct ListNode {
+     int val;
+     struct ListNode *next;
+ };
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+	if (head->val == NULL)
+		return NULL;
+	struct ListNode* cur = head;
+	struct ListNode* prev = NULL;
+	while (cur)
+	{
+		if (cur->val == val)
+		{
+			struct ListNode* next = cur->next;
+			if (prev == NULL)
+			{
+				head = cur->next;
+			}
+			else
+			{
+				prev->next = cur->next;
+			}
+			free(cur);
+			cur = next;
+		}
+		else
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+		return head;
+	}
